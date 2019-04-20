fec244d3a74fbbd8fbb72c4d5108eb467612d415
Tanapat Choochot <tanapat.pm@mail.kmutt.ac.rh>
2019-04-20 15:23:16 +0700

    add view all seats and rooms api

--- meetee_backend/app.js
+++ meetee_backend/app.js
@@ -20,6 +20,30 @@ app.get('/', (request, response) => {
     response.send('Meetee welcome!');
 });
 
+app.get('/view/rooms', (request, response) => {
+    knex('meetee.rooms')
+        .join('meetee.roomtypes', 'rooms.roomtype_id', '=', 'roomtypes.id')
+        .where('roomtypes.category_id', '1')
+        .select('rooms.id', 'rooms.code', 'roomtypes.name', 'roomtypes.price', 'roomtypes.capacity', 'rooms.floor')
+        .then((results) => {
+            response.send(results);
+        }).catch((error) => {
+            response.send(error);
+        })
+});
+
+app.get('/view/seats', (request, response) => {
+    knex('meetee.rooms')
+        .join('meetee.roomtypes', 'rooms.roomtype_id', '=', 'roomtypes.id')
+        .where('roomtypes.category_id', '2')
+        .select('rooms.id', 'rooms.code', 'roomtypes.name', 'roomtypes.price', 'roomtypes.capacity', 'rooms.floor')
+        .then((results) => {
+            response.send(results);
+        }).catch((error) => {
+            response.send(error);
+        })
+});
+
 app.listen(PORT, URL, () => {
     console.log(`Listening on PORT: ${PORT} > ${process.env.NODE_ENV} > ${process.env.DB_LOCAL_HOST}`);
 })
