3470224a10611b8a1c7af53c2c2103b258aa835d
Tanapat Choochot <tanapat.pm@mail.kmutt.ac.rh>
2019-04-20 14:45:52 +0700

    add config file for production mode

---
+++ meetee_backend/config/database-pg-rds.js
@@ -0,0 +1,16 @@
+require('dotenv').config({
+  path: './.env'
+});
+
+var knex = require('knex')({
+  client: process.env.DB_CLIENT,
+  version: '10.6',
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  }
+});
+
+module.exports = knex;
