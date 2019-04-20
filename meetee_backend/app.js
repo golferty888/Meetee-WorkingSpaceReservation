b6ed5205fd47e0c1a18f382f69c6def3836ccf42
Tanapat Choochot <tanapat.pm@mail.kmutt.ac.rh>
2019-04-20 15:26:51 +0700

    add api for reserving a room/seat

--- meetee_backend/app.js
+++ meetee_backend/app.js
@@ -46,6 +46,32 @@ app.post('/check/available', (request, response) => {
         })
 })
 
+app.post('/reserve', (request, response) => {
+    const userId = request.body.userId;
+    const roomId = request.body.roomId;
+    const startDate = request.body.startDate;
+    const endDate = request.body.endDate;    
+    var startTime = request.body.startTime;
+    startTime = startTime.substr(0,6) + '01';
+    const endTime = request.body.endTime;
+    knex('meetee.reservation')
+        .returning('id', 'room_id', 'user_id', 'start_date', 'start_time', 'end_date', 'end_time')
+        .insert([{
+            user_id: userId,
+            room_id: roomId,
+            start_date: startDate,
+            end_date: endDate,
+            start_time: startTime,
+            end_time: endTime
+        }])
+        .then((result) => {
+            response.send(result);
+        })
+        .catch((error) => {
+            response.send(error);
+        })
+});
+
 app.get('/view/rooms', (request, response) => {
     knex('meetee.rooms')
         .join('meetee.roomtypes', 'rooms.roomtype_id', '=', 'roomtypes.id')
