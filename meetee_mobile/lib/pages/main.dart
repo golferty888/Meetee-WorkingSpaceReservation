9a00fb174d04af8842800d9012ef89e97a4a67ed
Gulfz <flog-fy@hotmail.com>
2019-08-28 14:39:55 +0700

    add home page with navigator to null

---
+++ meetee_mobile/lib/pages/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: Colors.black,
+        scaffoldBackgroundColor: Colors.grey,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home page'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              alignment: AlignmentDirectional.center,
+              color: Colors.lightBlueAccent,
+              child: FlatButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => null,
+                    ),
+                  );
+                },
+                child: Text(
+                  'Reserve',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 36.0,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              alignment: AlignmentDirectional.center,
+              color: Colors.pinkAccent,
+              child: FlatButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => null,
+                    ),
+                  );
+                },
+                child: Text(
+                  'History',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 36.0,
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
