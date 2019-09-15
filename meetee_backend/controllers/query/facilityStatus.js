096dda55deecc12f062df140c587c6d0101246e2
Tanapat Choochot <tanapat.pm@mail.kmutt.ac.rh>
2019-09-15 23:25:03 +0700

    update unavailable query 'send FacList with status'

--- meetee_backend/controllers/query/facilityStatus.js
+++ meetee_backend/controllers/query/facilityStatus.js
@@ -6,27 +6,27 @@ exports.checkAvailability = (request, response) => {
     const endDate = request.body.endDate;
     const startTime = request.body.startTime;
     const endTime = request.body.endTime;
-    console.log(startTime + " " + endTime)
+    console.log(startTime + ' ' + endTime)
 
     const unavailableQuery =
-        knex("meetee.reservation as resv")
-        .distinct("resv.room_id")
-        .join("meetee.rooms as room", "resv.room_id", "=", "room.id")
-        .where("room.roomtype_id", "=", type)
-        .andWhere("resv.start_date", '=', startDate)
+        knex('meeteenew.reservation as resv')
+        .distinct('resv.facility_id')
+        .join('meeteenew.facility as facility', 'resv.facility_id', '=', 'facility.id')
+        .where('facility.facility_category_id', '=', type)
+        .andWhere('resv.start_date', '=', startDate)
         .andWhere(function () {
             this.orWhere(
-                knex.raw(`(TIME '${startTime}', TIME '${endTime}') OVERLAPS (resv.start_time, resv.end_time)`)
+                knex.raw(`(TIMESTAMP '${startTime}', TIME '${endTime}') OVERLAPS (resv.start_time, resv.end_time)`)
             )
         })
 
     const availableQuery =
         knex
-        .select('room.id as roomId', 'room.code as roomCode', 'room.floor as floor', 'room.roomtype_id as roomType')
-        .from("meetee.rooms as room")
-        .where("room.roomtype_id", "=", type)
-        .andWhere("room.id", "NOT IN", unavailableQuery)
-        .orderBy("room.id")
+        .select('facility.id as roomId', 'facility.code as roomCode', 'facility.floor as floor', 'facility.facility_category_id as roomType')
+        .from('meeteenew.facility as facility')
+        .where('facility.facility_category_id', '=', type)
+        .andWhere('facility.id', 'NOT IN', unavailableQuery)
+        .orderBy('facility.id')
         .then(data => response.json({
             successful: true,
             data
@@ -39,30 +39,103 @@ exports.checkAvailability = (request, response) => {
 
 exports.checkUnavailability = (request, response) => {
     const type = request.body.type;
-    const startDate = request.body.startDate;
-    const endDate = request.body.endDate;
-    const startTime = request.body.startTime;
-    const endTime = request.body.endTime;
-    console.log(startTime + " " + endTime)
+    const data = request.body
+    const startDate = data.startDate;
+    const startTime = data.startDate + ' ' + data.startTime
+    const endTime = data.endDate + ' ' + data.endTime
+    const status = 'Booked'
+    const user_id = data.userId
+    console.log(type)
+    console.log(startTime + ' ' + endTime)
 
     const unavailableQuery =
-        knex("meetee.reservation as resv")
-        .select('room.id as roomId', 'room.code as roomCode', 'room.floor as floor', 'room.roomtype_id as roomType')
-        .distinct("resv.room_id")
-        .join("meetee.rooms as room", "resv.room_id", "=", "room.id")
-        .where("room.roomtype_id", "=", type)
-        .andWhere("resv.start_date", '=', startDate)
+        knex('meeteenew.reservation as resv')
+        .distinct('resv.facility_id as facId')
+        .select('facility.code as facCode', 'facility.floor', 'facility.facility_category_id')
+        .join('meeteenew.facility as facility', 'resv.facility_id', '=', 'facility.id')
+        .where('facility.facility_category_id', '=', type)
+        .andWhereNot('resv.status', 'Cancelled')
+        // .andWhere('resv.start_date', '=', startDate)
         .andWhere(function () {
             this.orWhere(
-                knex.raw(`(TIME '${startTime}', TIME '${endTime}') OVERLAPS (resv.start_time, resv.end_time)`)
+                knex.raw(`(TIMESTAMP '${startTime}', TIMESTAMP '${endTime}') OVERLAPS (resv.start_time, resv.end_time)`)
             )
         })
-        .then(data => response.json({
-            successful: true,
-            data
-        }))
+        .then(data => response.send(data))
         .catch(error => {
             console.log(error)
             response.status(400).send('Bad request')
         })
+    // knex('meeteenew.reservation as resv')
+    // .distinct('resv.facility_id as facId')
+    // .select('facility.code as facCode', 'facility.floor', 'facility.facility_category_id')
+    // // for mutiple-choose .join('meeteenew.reservation_detail as detail', 'resv.id', '=', 'detail.reservation_id')
+    // .join('meeteenew.facility as facility', 'resv.facility_id', '=', 'facility.id')
+    // .where('facility.facility_category_id', '=', type)
+    // // .andWhere('resv.start_date', '=', startDate)
+    // .andWhere(function () {
+    //     this.orWhere(
+    //         knex.raw(`(TIMESTAMP '${startTime}', TIMESTAMP '${endTime}') OVERLAPS (resv.start_time, resv.end_time)`)
+    //     )
+    // })
+}
+
+exports.checkUnavailabilityAllCategory = (request, response) => {
+    const data = request.body
+    const startDate = data.startDate;
+    const startTime = data.startDate + ' ' + data.startTime
+    const endTime = data.endDate + ' ' + data.endTime
+    const status = 'Booked'
+    const user_id = data.userId
+    console.log(startTime + ' ' + endTime)
+
+    const allFacilityQuery =
+        knex('meeteenew.facility as facility')
+        .select('facility.id as facId', 'facility.code as facCode', 'facility.floor', 'facility.facility_category_id')
+        .orderBy('facId');
+
+    const unavailableQuery =
+        knex('meeteenew.reservation as resv')
+        .distinct('resv.facility_id as facId')
+        .select('facility.code as facCode', 'facility.floor', 'facility.facility_category_id')
+        .join('meeteenew.facility as facility', 'resv.facility_id', '=', 'facility.id')
+        .whereNot('resv.status', 'Cancelled')
+        .andWhere(function () {
+            this.orWhere(
+                knex.raw(`(TIMESTAMP '${startTime}', TIMESTAMP '${endTime}') OVERLAPS (resv.start_time, resv.end_time)`)
+            )
+        })
+        .orderBy('facId');
+
+    allFacilityQuery.then(facAll => {
+        assignStatusFacList(facAll)
+        unavailableQuery.then(facUna => {
+                updateStatusFacList(facAll, facUna)
+                response.send(facAll)
+            })
+            .catch(error => {
+                console.log(error)
+                response.status(400).send('Bad request')
+            })
+    })
+}
+
+function assignStatusFacList(facList) {
+    console.log("FacList Length: " + facList.length)
+    for (i = 0; i < facList.length; i++) {
+        facList[i] = Object.assign(facList[i], {
+            status: "available"
+        })
+    }
+}
+
+function updateStatusFacList(facAll, facUna) {
+    console.log("UnavailableList: " + JSON.stringify(facUna))
+    for (i = 0; i < facUna.length; i++) {
+        for (j = 0; j < facAll.length; j++) {
+            if (facUna[i].facCode == facAll[j].facCode) {
+                facAll[j].status = "unavailable"
+            }
+        }
+    }
 }
