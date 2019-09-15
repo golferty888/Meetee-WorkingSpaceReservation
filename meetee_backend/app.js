7d5d312e8207776c468823505afdd368376c63c2
Tanapat Choochot <tanapat.pm@mail.kmutt.ac.rh>
2019-09-15 23:28:13 +0700

    update reserv-trigger IN/UP/DE through Socket.io to Front-end

--- meetee_backend/app.js
+++ meetee_backend/app.js
@@ -1,5 +1,6 @@
 const dotenv = require("dotenv").config();
 const express = require("express");
+const pg = require('pg')
 var http = require("http");
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -30,25 +31,61 @@ const facStatus = require('./controllers/query/facilityStatus')
 const user = require('./controllers/query/user')
 const reservation = require('./controllers/query/reservation')
 
-// const roomQuery = require('./controllers/query/room')
-
 app.get('/fac', facility.getAllFacility)
-app.get('/fac/class/:id', facility.getFacilityCategory)
-app.get('/fac/:id', facility.getFacilityCategoryEquipment)
+// app.get('/fac/class/:id', facility.getFacilityCategory)
+app.get('/fac/class/:id', facility.getFacilityFromClass)
+app.get('/fac/category', facility.getAllFacilityCategory)
+// app.get('/fac/:id', equipment.getFacilityCategoryEquipment)
 app.post('/user/history', user.getReservationHistory)
 app.post('/check/available', facStatus.checkAvailability)
 app.post('/check/unavailable', facStatus.checkUnavailability)
+app.post('/check/unavailable/all', facStatus.checkUnavailabilityAllCategory)
 app.post('/reserve', reservation.reserve)
 app.get('/reservations', reservation.getAllReservations)
-// app.get('/type/room', roomQuery.getRoomTypeList)
-// app.get('/type/seat', roomQuery.getSeatTypeList)
+
+app.get('/test', async (request, response) => {
+  const data = await pgClient.query('SELECT * FROM meeteenew.reservation')
+  response.send({
+    table: data.rows
+  })
+})
 
 app.use("*", function (request, response) {
   response.status(404).send('404, Not found');
 });
 
+const connectionString = process.env.POSTGRES_CONNECTION_URL
+const pgClient = new pg.Client(connectionString);
+
+pgClient.connect();
+
+const query = pgClient.query("LISTEN events")
+
 server.listen(PORT, URL, () => {
   console.log(
     `✔ Listening on PORT: ${server.address().port} > ${process.env.NODE_ENV} > ${server.address().address}`
   );
+});
+
+const io = require('socket.io')(server);
+
+io.on("connection", (socket) => {
+  console.log("userId: " + socket.id + " connected.")
+
+  socket.on("disconnect", socket => {
+    console.log("!userId: " + socket.id + " disconnected.")
+  })
+})
+
+pgClient.on("notification", async (data) => {
+  const payload = data.payload
+  console.log("data.payload: , " + data.payload)
+  io.emit("reservation_trigger", payload)
+})
+
+pgClient.on('error', function (err, client) {
+  console.log('pg::error', {
+    err: err,
+    client: client
+  });
 });
