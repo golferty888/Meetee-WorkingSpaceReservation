fe4088cc99363fc438b2ffa47e6065957988d2cd
Gulfz <flog-fy@hotmail.com>
2019-09-26 23:47:17 +0700

    remove unnecessary rendering.dart

--- meetee_mobile/lib/pages/customerDemand.dart
+++ meetee_mobile/lib/pages/customerDemand.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
