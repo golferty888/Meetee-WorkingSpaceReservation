226bb40153f549fbdbbe23461b522d6f9568e838
Tanapat Choochot <tanapat.pm@mail.kmutt.ac.th>
2019-10-07 00:41:38 +0700

    add reservation ID to history API

--- meetee_backend/controllers/query/user.js
+++ meetee_backend/controllers/query/user.js
@@ -5,7 +5,7 @@ const {
 
 exports.getReservationHistoryList = (request, response) => {
     const id = request.body.userId;
-    knex.select('fac.id', 'cate.name', 'fac.code', 'fac.floor', 'resv.start_time', 'resv.end_time', 'resv.status', 'cate.price')
+    knex.select('resv.id as reservId' , 'fac.id as facId', 'cate.name', 'fac.code', 'fac.floor', 'resv.start_time', 'resv.end_time', 'resv.status', 'cate.price')
         .from('meeteenew.user as user')
         .join('meeteenew.reservation as resv', 'user.id', '=', 'resv.user_id')
         .join('meeteenew.facility as fac', 'resv.facility_id', '=', 'fac.id')
