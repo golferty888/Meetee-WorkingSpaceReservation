90cb9a2221258d79e05eb09031878656ff77cc36
Tanapat Choochot <tanapat.pm@mail.kmutt.ac.th>
2019-10-10 00:23:03 +0700

    fix timezone

--- meetee_backend/config/knexfile.js
+++ meetee_backend/config/knexfile.js
@@ -13,12 +13,12 @@ module.exports = {
             database: 'sit',
             schema: 'meeteenew',
             charset: 'utf8',
-            timezone: '+07'
+            // timezone: '+07'
         },
         pg_connection: 'postgres://meetee_admin:meetee_admin@127.0.0.1:5432/sit',
         pool: {
             afterCreate: function (connection, callback) {
-                connection.query(`SET timezone = 'UTC-07';`, function (err) {
+                connection.query(`SET timezone = 'UTC+00';`, function (err) {
                     callback(err, connection);
                 });
             }
@@ -31,13 +31,12 @@ module.exports = {
             host: process.env.DB_HOST,
             user: process.env.DB_USERNAME,
             password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME,
-            timezone: '+07'
+            database: process.env.DB_NAME
         },
         pg_connection: `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
         pool: {
             afterCreate: function (connection, callback) {
-                connection.query(`SET timezone = 'UTC-07';`, function (err) {
+                connection.query(`SET timezone = 'UTC+00';`, function (err) {
                     callback(err, connection);
                 });
             }
