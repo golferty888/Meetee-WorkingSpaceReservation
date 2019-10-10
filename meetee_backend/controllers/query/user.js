9f7f36e8ad8761ebcaa37eb47da69fe8e61aa740
Tanapat Choochot <tanapat.pm@mail.kmutt.ac.th>
2019-10-10 22:11:55 +0700

    update data in history

--- meetee_backend/controllers/query/user.js
+++ meetee_backend/controllers/query/user.js
@@ -7,7 +7,18 @@ exports.getReservationHistoryList = (request, response) => {
     console.log("getHistory")
     const id = request.body.userId;
     knex('meeteenew.users as user')
-        .select(knex.raw(`resv.id as reservId, fac.id as facId, cate.name, fac.code, fac.floor, resv.start_time, resv.end_time, cate.price as price_per_hour, meeteenew.hour_cal(resv.start_time, resv.end_time) as hour, meeteenew.price_over_hours(cate.price, resv.start_time, resv.end_time) as total_price, resv.status`))
+        .select(knex.raw(`
+        resv.id as reservId, 
+        fac.id as facId, 
+        cate.name, 
+        fac.code, 
+        fac.floor, 
+        cate.price as price, 
+        resv.start_time as date, 
+        meeteenew.time_period(resv.start_time, resv.end_time) as period, 
+        meeteenew.hour_cal(resv.start_time, resv.end_time) as hour, 
+        meeteenew.price_over_hours(cate.price, resv.start_time, resv.end_time) as total_price, 
+        resv.status`))
         .join('meeteenew.reservation as resv', 'user.id', '=', 'resv.user_id')
         .join('meeteenew.facility as fac', 'resv.facility_id', '=', 'fac.id')
         .join('meeteenew.facility_category as cate', 'fac.facility_category_id', '=', 'cate.id')
