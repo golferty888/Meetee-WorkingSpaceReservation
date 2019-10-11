0a95cd020c87c08bf5864a0053ac9615dd9cd753
Tanapat Choochot <tanapat.pm@mail.kmutt.ac.th>
2019-10-12 00:42:05 +0700

    update

--- meetee_backend/migrations/sqls/20190912160657-realtime-facility-status-down.sql
+++ meetee_backend/migrations/sqls/20190912160657-realtime-facility-status-down.sql
@@ -1,4 +1,6 @@
 /* Replace with your SQL commands */
+DROP TABLE IF EXISTS meeteenew.reservation;
+DROP TABLE IF EXISTS meeteenew.reserv_audit;
 DROP FUNCTION IF EXISTS meeteenew.date_format(timestamp with time zone, timestamp with time zone);
 DROP FUNCTION IF EXISTS meeteenew.time_period(timestamp with time zone, timestamp with time zone);
 DROP FUNCTION IF EXISTS meeteenew.hour_cal(timestamp with time zone, timestamp with time zone);
@@ -6,5 +8,3 @@ DROP FUNCTION IF EXISTS meeteenew.price_over_hours(numeric, timestamp with time 
 DROP FUNCTION IF EXISTS meeteenew.get_reserv_history(numeric, timestamp with time zone, timestamp with time zone);
 DROP TRIGGER IF EXISTS notify_reservation_event ON meeteenew.reservation CASCADE;
 DROP FUNCTION IF EXISTS meeteenew.notify_trigger();
-DROP TABLE IF EXISTS meeteenew.reservation;
-DROP TABLE IF EXISTS meeteenew.reserv_audit;
