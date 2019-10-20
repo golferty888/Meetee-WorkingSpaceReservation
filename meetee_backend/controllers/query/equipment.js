1f938fa572e93ca6e96e2d7ea17692237846775b
Tanapat Choochot <tanapat.pm@mail.kmutt.ac.th>
2019-10-20 21:34:59 +0700

    UPDATE use * instead specific columns

--- meetee_backend/controllers/query/equipment.js
+++ meetee_backend/controllers/query/equipment.js
@@ -6,13 +6,10 @@ const pool = new Pool({
 exports.getFacilityCategoryDetail = (request, response) => {
   const cateId = parseInt(request.params.id);
   console.log("--> Request /fac/type/:id/detail " + "params.id=" + cateId);
-  const queryText = `SELECT cateId, cateName, capacity, price, link_url, array_agg(json_build_object('eqid', eqid,'eqname', eqname)) eqList
-    from meeteenew.view_faccate_detail
-    WHERE cateId = $1
-    group by cateId, catename, capacity, price, link_url;`;
-  const queryValue = [cateId];
-  pool.query(queryText, queryValue, (error, results) => {
-    const eqList = [];
+  const statement = `SELECT * from meeteenew.view_faccate_detail
+    WHERE cateId = $1`;
+  const value = [cateId];
+  pool.query(statement, value, (error, results) => {
     if (error) {
       response.status(500).send("Database Error");
     }
--- meetee_backend/controllers/query/facility.js
+++ meetee_backend/controllers/query/facility.js
@@ -18,11 +18,11 @@ exports.getFacilityCategoriesFromType = (request, response) => {
   const typeId = request.params.id;
   console.log("--> Request /fac/type/:id: " + "params.id=" + typeId);
 
-  const statement = `select v1.cateId, v1.cateName, v1.price, v1.capacity, v1.link_url, v1.typeId, v1.typeName from meeteenew.view_factype_detail as v1
-    where v1.typeId = ${typeId}
-    order by v1.cateId`;
+  const statement = `select * from meeteenew.view_factype_detail as v
+    where v.typeId = $1`;
+  const value = [typeId];
 
-  pool.query(statement, (error, result) => {
+  pool.query(statement, value, (error, result) => {
     if (error) {
       response.status(500).send("Database Error");
       throw error;
