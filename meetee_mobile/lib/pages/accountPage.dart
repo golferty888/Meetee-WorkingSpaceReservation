c53ddbebc59263db2ff41debd1deb2b5df79574c
Gulfz <flog-fy@hotmail.com>
2020-01-08 17:34:02 +0700

    change account page to listview

--- meetee_mobile/lib/pages/accountPage.dart
+++ meetee_mobile/lib/pages/accountPage.dart
@@ -22,245 +22,243 @@ class _AccountPageState extends State<AccountPage> {
       appBar: AppBar(
         title: Text('Account'),
       ),
-      body: Container(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            SizedBox(
-              height: 8,
-            ),
-            Container(
-              padding: EdgeInsets.fromLTRB(16, 24, 16, 24),
-              color: Colors.white,
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    width: 64.0,
-                    height: 64.0,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      image: DecorationImage(
-                        fit: BoxFit.fill,
-                        image: AssetImage(
-                          'images/placeholder_image.png',
-                        ),
+      body: ListView(
+        shrinkWrap: true,
+        children: <Widget>[
+          SizedBox(
+            height: 8,
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(16, 24, 16, 24),
+            color: Colors.white,
+            child: Row(
+              children: <Widget>[
+                Container(
+                  width: 64.0,
+                  height: 64.0,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      fit: BoxFit.fill,
+                      image: AssetImage(
+                        'images/placeholder_image.png',
                       ),
                     ),
                   ),
-                  SizedBox(
-                    width: 16,
-                  ),
-                  Text(
-                    widget.userName,
-                    style: TextStyle(
-                      fontSize: 20,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            Container(
-              padding: EdgeInsets.fromLTRB(16, 16, 16, 16),
-              color: Colors.white,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        'Email',
-                        style: TextStyle(
-                          fontSize: 16,
-                        ),
-                      ),
-                      Row(
-                        children: <Widget>[
-                          Text(
-                            'Not verified',
-                            style: TextStyle(fontSize: 16, color: Colors.red),
-                          ),
-                          SizedBox(
-                            width: 8,
-                          ),
-                          Icon(
-                            Icons.arrow_forward_ios,
-                            color: Colors.black,
-                            size: 16,
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 8,
-                  ),
-                  Text(
-                    'placeholder_email@gmail.com',
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.black54,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 2,
-            ),
-            Container(
-              padding: EdgeInsets.fromLTRB(16, 16, 16, 16),
-              color: Colors.white,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        'Change password',
-                        style: TextStyle(
-                          fontSize: 16,
-                        ),
-                      ),
-                      Icon(
-                        Icons.arrow_forward_ios,
-                        color: Colors.black,
-                        size: 16,
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 2,
-            ),
-            Container(
-              padding: EdgeInsets.fromLTRB(16, 16, 16, 16),
-              color: Colors.white,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        'Language',
-                        style: TextStyle(
-                          fontSize: 16,
-                        ),
-                      ),
-                      Row(
-                        children: <Widget>[
-                          Text(
-                            'English',
-                            style: TextStyle(fontSize: 16, color: Colors.black),
-                          ),
-                          SizedBox(
-                            width: 8,
-                          ),
-                          Icon(
-                            Icons.arrow_forward_ios,
-                            color: Colors.black,
-                            size: 16,
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(left: 16, top: 24, bottom: 8),
-              height: 16,
-              child: Text(
-                'Notification'.toUpperCase(),
-                style: TextStyle(
-                  color: Colors.black54,
                 ),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.fromLTRB(16, 16, 16, 16),
-              color: Colors.white,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        'Alert time',
-                        style: TextStyle(
-                          fontSize: 16,
-                        ),
-                      ),
-                      Row(
-                        children: <Widget>[
-                          Text(
-                            'Before 3 minutes',
-                            style: TextStyle(fontSize: 16, color: Colors.black),
-                          ),
-                          SizedBox(
-                            width: 8,
-                          ),
-                          Icon(
-                            Icons.arrow_forward_ios,
-                            color: Colors.black,
-                            size: 16,
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            Expanded(
-              child: Container(),
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: 80.0,
-              padding: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 16.0),
-//              color: Colors.white,
-              child: OutlineButton(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8.0),
+                SizedBox(
+                  width: 16,
                 ),
-                borderSide: BorderSide(
-                  color: Theme.of(context).primaryColor,
+                Text(
+                  widget.userName,
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(16, 16, 16, 16),
+            color: Colors.white,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      'Email',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Text(
+                          'Not verified',
+                          style: TextStyle(fontSize: 16, color: Colors.red),
+                        ),
+                        SizedBox(
+                          width: 8,
+                        ),
+                        Icon(
+                          Icons.arrow_forward_ios,
+                          color: Colors.black,
+                          size: 16,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  'placeholder_email@gmail.com',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black54,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 2,
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(16, 16, 16, 16),
+            color: Colors.white,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      'Change password',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                    Icon(
+                      Icons.arrow_forward_ios,
+                      color: Colors.black,
+                      size: 16,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 2,
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(16, 16, 16, 16),
+            color: Colors.white,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      'Language',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Text(
+                          'English',
+                          style: TextStyle(fontSize: 16, color: Colors.black),
+                        ),
+                        SizedBox(
+                          width: 8,
+                        ),
+                        Icon(
+                          Icons.arrow_forward_ios,
+                          color: Colors.black,
+                          size: 16,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 16, top: 24, bottom: 8),
+            height: 16,
+            child: Text(
+              'Notification'.toUpperCase(),
+              style: TextStyle(
+                color: Colors.black54,
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(16, 16, 16, 16),
+            color: Colors.white,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      'Alert time',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Text(
+                          'Before 3 minutes',
+                          style: TextStyle(fontSize: 16, color: Colors.black),
+                        ),
+                        SizedBox(
+                          width: 8,
+                        ),
+                        Icon(
+                          Icons.arrow_forward_ios,
+                          color: Colors.black,
+                          size: 16,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 80.0,
+            padding: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 16.0),
+//            color: Colors.white,
+            child: OutlineButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              borderSide: BorderSide(
+                color: Theme.of(context).primaryColor,
 //                  color: Colors.red,
-                ),
-                onPressed: () async {
-                  final prefs = await SharedPreferences.getInstance();
-                  prefs.remove('userName');
-                  prefs.remove('passWord');
-                  Navigator.of(context).pushNamedAndRemoveUntil(
-                      '/login', (Route<dynamic> route) => false);
-                },
-                child: Container(
-                  padding: EdgeInsets.symmetric(vertical: 8.0),
-                  width: MediaQuery.of(context).size.width,
-                  child: Text(
-                    'LOG OUT'.toUpperCase(),
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Theme.of(context).primaryColor,
-                      letterSpacing: 2.0,
-                      fontWeight: FontWeight.bold,
-                    ),
+              ),
+              onPressed: () async {
+                final prefs = await SharedPreferences.getInstance();
+                prefs.remove('userName');
+                prefs.remove('passWord');
+                Navigator.of(context).pushNamedAndRemoveUntil(
+                    '/login', (Route<dynamic> route) => false);
+              },
+              child: Container(
+                padding: EdgeInsets.symmetric(vertical: 8.0),
+                width: MediaQuery.of(context).size.width,
+                child: Text(
+                  'LOG OUT'.toUpperCase(),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    letterSpacing: 2.0,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
